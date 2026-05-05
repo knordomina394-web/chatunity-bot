b2e9dee0b7c03b56ae7208f690c3820f99e092e0
knordomina394-web <knordomina394@gmail.com>
2026-05-05 12:39:10 +0200

    Update config.js

--- config.js
+++ config.js
@@ -19,8 +19,8 @@ global.wait = 'ⓘ 𝐂𝐚𝐫𝐢𝐜𝐚𝐦𝐞𝐧𝐭𝐨 ...';
 
 
 global.owner = [
-  ['393773842461', '𝐂𝐡𝐚𝐭𝐔𝐧𝐢𝐭𝐲', true],
-  ['xxxxxxxxxx'], // Numero, Nome, True (attenzione alla sintassi deve essere identico a sopra!)
+  ['393926427789', '𝐂𝐡𝐚𝐭𝐔𝐧𝐢𝐭𝐲', true],
+  ['393514960126'], // Numero, Nome, True (attenzione alla sintassi deve essere identico a sopra!)
   ['xxxxxxxxxx'],
   ['xxxxxxxxxx'],
   ['xxxxxxxxxx'],
